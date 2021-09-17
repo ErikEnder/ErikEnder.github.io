22c3c4ce772835dc89dd2dc1387408ce8b29ecff
Erik Ender <45837117+ErikEnder@users.noreply.github.com>
2021-09-16 23:57:15 -0500

    Optimization Changes
    Removed some pointless code/files, changed how the JavaScript loads using jQuery and moving the scripts to the bottom of the DOM so that hopefully the page will load faster when accessed.

--- animation.js
+++ animation.js
@@ -1,38 +1,40 @@
 var id = null;
 
-function myMove() {
-  var barHeight = document.getElementById("top-bar").clientHeight;
-  var barWidth = document.getElementById("top-bar").clientWidth;
-  var cornerWidth = document.querySelector(".corner").clientWidth;
+  function myMove() {
+    var barHeight = document.getElementById("top-bar").clientHeight;
+    var barWidth = document.getElementById("top-bar").clientWidth;
+    var cornerWidth = document.querySelector(".corner").clientWidth;
 
-  var windowHeight = window.innerHeight;
+    var windowHeight = window.innerHeight;
 
 
-  var elem = document.getElementsByClassName("dog-container")[0];
-  var pos = elem.style.left;
-  var intClientWidth = document.clientWidth;
+    var elem = document.getElementsByClassName("dog-container")[0];
+    var pos = elem.style.left;
+    var intClientWidth = document.clientWidth;
 
-  clearInterval(id);
-  id = setInterval(frameF, 10);
+    clearInterval(id);
+    id = setInterval(frameF, 10);
 
-  function frameF() {
-    if (pos == (barWidth - cornerWidth)) {
-      elem.style.transform = 'scaleX(-1)';
-      id = setInterval(frameB, 10);
-    } else {
-      pos++;
-      elem.style.left = pos + 'px';
+    function frameF() {
+      if (pos == (barWidth - cornerWidth)) {
+        elem.style.transform = 'scaleX(-1)';
+        id = setInterval(frameB, 10);
+      } else {
+        pos++;
+        elem.style.left = pos + 'px';
+      }
     }
-  }
 
-  function frameB() {
-      if (pos == 100) {
-      elem.style.display = 'none';
-      id = setInterval(frameF, 10);
-    } else {
-      pos--;
-      elem.style.left = pos + 'px';
+    function frameB() {
+        if (pos == 100) {
+        elem.style.display = 'none';
+        id = setInterval(frameF, 10);
+      } else {
+        pos--;
+        elem.style.left = pos + 'px';
 
     }
   }
 }
+
+$(document).ready(myMove);
