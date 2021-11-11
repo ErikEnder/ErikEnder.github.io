ea43b5013a9fb762f478c793fcd093c9fa28b211
Erik Ender <45837117+ErikEnder@users.noreply.github.com>
2021-11-10 18:43:58 -0600

    Website Update from Desktop
    Setting up repository on Desktop for easier uploading.

--- jqueryanimate.js
+++
@@ -1,8 +0,0 @@
-var barWidth = $('#top-bar').clientWidth;
-var cornerWidth = $('.corner').clientWidth;
-
-var leftMove = barWidth - cornerWidth;
-
-$(document).ready(function(){
-  $('.dog-container').animate({left: '90%'}, 1000);
-});
